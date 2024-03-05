ab6fd002d9dc0d808b10db6497e572c743e0bc00
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-05 11:54:16 +0100

    Unittest File Storage class

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,6 +3,7 @@
 import json
 import unittest
 
+from models import storage
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -11,63 +12,63 @@ class TestFileStorage(unittest.TestCase):
     """Tests File Storage class."""
     def test_file_path(self):
         """Test __file_path"""
-        storage = FileStorage()
-        expected_file = "file.json"
-        self.assertEqual(storage._FileStorage__file_path, expected_file)
+        file_storage = FileStorage()
+        expected_file = 'file.json'
+        self.assertEqual(file_storage._FileStorage__file_path, expected_file)
 
     def test_objects(self):
         """Test __objects"""
-        storage = FileStorage()
-        self.assertIsInstance(storage._FileStorage__objects, dict)
+        file_storage = FileStorage()
+        self.assertIsInstance(file_storage._FileStorage__objects, dict)
 
     def test_all(self):
         """Test all()"""
-        storage = FileStorage()
-        new_dict = storage.all()
+        file_storage = FileStorage()
+        new_dict = file_storage.all()
         self.assertIsInstance(new_dict, dict)
-        self.assertIs(new_dict, storage._FileStorage__objects)
+        self.assertIs(new_dict, file_storage._FileStorage__objects)
 
     def test_new(self):
         """Test new()"""
-        storage = FileStorage()
+        file_storage = FileStorage()
         my_model = BaseModel()
-        storage.new(my_model)
-        self.assertIn("BaseModel." + my_model.id, storage._FileStorage__objects)
+        file_storage.new(my_model)
+        self.assertIn("BaseModel." + my_model.id, file_storage._FileStorage__objects)
 
     def test_save(self):
         """Test save()"""
-        storage = FileStorage()
+        file_storage = FileStorage()
         my_model = BaseModel()
-        storage.new(my_model)
-        storage.save()
+        file_storage.new(my_model)
+        file_storage.save()
         with open("file.json", "r") as f:
-            self.assertNotEqual(json.load(f), storage._FileStorage__objects)
+            self.assertNotEqual(json.load(f), file_storage._FileStorage__objects)
 
     def test_reload(self):
         """Test reload()"""
-        storage = FileStorage()
+        file_storage = FileStorage()
         my_model = BaseModel()
-        storage.new(my_model)
-        storage.save()
-        storage.reload()
-        self.assertIn("BaseModel." + my_model.id, storage._FileStorage__objects)
+        file_storage.new(my_model)
+        file_storage.save()
+        file_storage.reload()
+        self.assertIn("BaseModel." + my_model.id, file_storage._FileStorage__objects)
         with open("file.json", "r") as f:
-            self.assertNotEqual(json.load(f), storage._FileStorage__objects)
+            self.assertNotEqual(json.load(f), file_storage._FileStorage__objects)
 
     def test__init__(self):
         """Test __init__()"""
-        storage = FileStorage()
-        self.assertIsInstance(storage, FileStorage)
-        self.assertIsInstance(storage._FileStorage__objects, dict)
+        file_storage = FileStorage()
+        self.assertIsInstance(file_storage, FileStorage)
+        self.assertIsInstance(file_storage._FileStorage__objects, dict)
 
     def test_save2(self):
         """Test save()"""
-        storage = FileStorage()
+        file_storage = FileStorage()
         my_model = BaseModel()
-        storage.new(my_model)
-        storage.save()
+        file_storage.new(my_model)
+        file_storage.save()
         with open("file.json", "r") as f:
-            self.assertNotEqual(json.load(f), storage._FileStorage__objects)
+            self.assertNotEqual(json.load(f), file_storage._FileStorage__objects)
 
 if __name__ == "__main__":
     unittest.main()
