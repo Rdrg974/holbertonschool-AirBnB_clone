22778f115aca44f64b5aaa8020272bb8ca7246e6
kjuarez38 <kaisjuarez@gmail.com>
2024-03-06 16:46:56 +0100

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -28,17 +28,32 @@ class TestBaseModel(unittest.TestCase):
         my_model.name = "My First Model"
         my_model.my_number = 89
         my_model_json = my_model.to_dict()
+<<<<<<< HEAD
         self.assertEqual(my_model_json["id"], my_model.id)
         self.assertEqual(my_model_json["created_at"],
                          my_model.created_at.isoformat())
+=======
+        id1 = my_model_json["id"]
+        id2 = my_model.id
+        created1 = my_model_json["created_at"]
+        created2 = my_model.created_at.isoformat()
+        self.assertNotEqual(id1, id2)
+        self.assertEqual(created1, created2)
+>>>>>>> 2816098 (test_base_model.py)
 
     def test__str__(self):
         """Test __str__()"""
         my_model = BaseModel()
         my_model.name = "My First Model"
         my_model.my_number = 89
+<<<<<<< HEAD
         self.assertEqual(str(my_model),
                          f"[BaseModel] ({my_model.id}) {my_model.__dict__}")
+=======
+        s1 = str(my_model)
+        s2 = "[BaseModel] ({}) {}".format(my_model.id, my_model.__dict__)
+        self.assertEqual(s1, s2)
+>>>>>>> 2816098 (test_base_model.py)
 
     def test_init(self):
         """Test __init__()"""
@@ -50,5 +65,5 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(my_model.updated_at, datetime.datetime)
 
 
-if __name__ == "__main__":
-    unittest.main()
+    if __name__ == "__main__":
+        unittest.main()
