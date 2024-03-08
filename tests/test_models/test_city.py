0c21bf43adcad38dc0679a80ac1fa9b778ad2db5
Abdou Rodrigue Hassany Mohamed <hassany.rodrigue@gmail.com>
2024-03-08 18:40:38 +0100

    Unittest for new classes : Place, State, City, Amenity and Review

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Module for City unittests."""
+import unittest
+
+from models.base_model import BaseModel
+from models.city import City
+
+
+class TestCity(unittest.TestCase):
+    """Class to test the City class."""
+
+    def setUp(self):
+        """Create an instance of City to use in tests."""
+        self.city = City()
+
+    def tearDown(self):
+        """Delete the instance of City after tests."""
+        del self.city
+
+    def test_city_inherits_from_base_model(self):
+        """Test that City is a subclass of BaseModel."""
+        self.assertIsInstance(self.city, BaseModel)
+
+    def test_city_has_state_id_attribute(self):
+        """Test that City has a class attribute state_id."""
+        self.assertTrue(hasattr(City, "state_id"))
+
+    def test_state_id_is_string(self):
+        """Test that the type of state_id is str."""
+        self.assertEqual(type(City.state_id), str)
+
+    def test_state_id_is_empty_string(self):
+        """Test that the default value of state_id is an empty string."""
+        self.assertEqual(City.state_id, "")
+
+    def test_city_has_name_attribute(self):
+        """Test that City has a class attribute name."""
+        self.assertTrue(hasattr(City, "name"))
+
+    def test_name_is_string(self):
+        """Test that the type of name is str."""
+        self.assertEqual(type(City.name), str)
+
+    def test_name_is_empty_string(self):
+        """Test that the default value of name is an empty string."""
+        self.assertEqual(City.name, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_review.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Module for Review unittests."""
+import unittest
+
+from models.base_model import BaseModel
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    """Class to test the Review class."""
+
+    def setUp(self):
+        """Create an instance of Review to use in tests."""
+        self.review = Review()
+
+    def tearDown(self):
+        """Delete the instance of Review after tests."""
+        del self.review
+
+    def test_review_inherits_from_base_model(self):
+        """Test that Review is a subclass of BaseModel."""
+        self.assertIsInstance(self.review, BaseModel)
+
+    def test_review_has_attributes(self):
+        """Test that Review has the required class attributes."""
+        attributes = ["place_id", "user_id", "text"]
+        for attr in attributes:
+            self.assertTrue(hasattr(Review, attr))
+
+    def test_attributes_are_correct_type(self):
+        """Test that the type of the attributes is correct."""
+        self.assertEqual(type(Review.place_id), str)
+        self.assertEqual(type(Review.user_id), str)
+        self.assertEqual(type(Review.text), str)
+
+    def test_attributes_are_default_values(self):
+        """Test that the default value of the attributes is correct."""
+        self.assertEqual(Review.place_id, "")
+        self.assertEqual(Review.user_id, "")
+        self.assertEqual(Review.text, "")
+
+
+if __name__ == "__main__":
+    unittest.main()
